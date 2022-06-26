dc8f50eb6fce4c563a9b5f1099aec19c5807b94a
ZbkSou <zhoubk1451@163.com>
2022-06-26 23:11:43 +0800

    update: 增加 enumerable set

--- contracts/AccessDemo.sol
+++ contracts/AccessDemo.sol
@@ -1,7 +1,19 @@
 pragma solidity ^0.8.0;
 
-contract AccessDemo is AccessControl{
-    function AccessDemo(){
+import ".\access\AccessControl.sol";
 
+// AccessControl 使用
+contract AccessDemo is AccessControl {
+
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    function AccessDemo(){
+        _setRoleAdmin(MINTER_ROLE, DEFAULT_ADMIN_ROLE);
     }
+
+    function getMinterRoleBytes() view returns(bytes32){
+        return MINTER_ROLE;
+    }
+
+
 }
