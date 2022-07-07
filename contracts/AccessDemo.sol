c4ae723c1ad17f805384cc7e7c3e7e59fa181fcf
ZbkSou <zhoubk1451@163.com>
2022-07-08 00:14:07 +0800

    update: 增加 demo erc165

--- contracts/AccessDemo.sol
+++ contracts/AccessDemo.sol
@@ -5,15 +5,25 @@ import ".\access\AccessControl.sol";
 // AccessControl 使用
 contract AccessDemo is AccessControl {
 
+
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
+    string private content;
     function AccessDemo(){
         _setRoleAdmin(MINTER_ROLE, DEFAULT_ADMIN_ROLE);
+        _setupRole(DEFAULT_ADMIN_ROLE,_msgSender());
     }
 
     function getMinterRoleBytes() view returns(bytes32){
         return MINTER_ROLE;
     }
 
+    function setContent(string memory text) external onlyRole(MINTER_ROLE) {
+        content = text;
+    }
+
+    function getContent() external view returns(string){
+        return content;
+    }
 
 }
